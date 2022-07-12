99f58dc0b0273732888b0b9ef97abbf60239b1a0
daikoku <21cm0148@jec.ac.jp>
2022-07-12 14:44:55 +0900

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by cmStudent on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
